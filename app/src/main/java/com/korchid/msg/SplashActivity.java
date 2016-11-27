1117df2076d5d184088ec34745328b1a393a4853
Macfield <rladudals02@naver.com>
2016-11-28 01:16:58 +0900

    Add Loading

---
+++ app/src/main/java/com/korchid/msg/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.korchid.msg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+/**
+ * Created by mac0314 on 2016-11-28.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Log.d("Loading", "SplashActivity Ok");
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
