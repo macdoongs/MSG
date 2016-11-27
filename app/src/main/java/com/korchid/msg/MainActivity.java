5f480f95146ccf3053643116cf5e8f5e8b19f6ff
Macfield <rladudals02@naver.com>
2016-11-28 00:57:12 +0900

    Android blank project

---
+++ app/src/main/java/com/korchid/msg/MainActivity.java
@@ -0,0 +1,13 @@
+package com.korchid.msg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
