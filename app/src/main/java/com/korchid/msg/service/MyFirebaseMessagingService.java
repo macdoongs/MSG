859fd960d82ed527d81be4e2490203c45ceba59c
Macfield <rladudals02@naver.com>
2016-11-29 01:06:37 +0900

    Add KakaoAPI

---
+++ app/src/main/java/com/korchid/msg/service/MyFirebaseMessagingService.java
@@ -0,0 +1,17 @@
+package com.korchid.msg.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    protected Intent zzF(Intent intent) {
+        return null;
+    }
+}
